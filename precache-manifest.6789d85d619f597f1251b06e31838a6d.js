124cca4bf8a40370af3b6772ab7eb19b08f6c890
EC2 Default User <ec2-user@ip-172-31-37-125.us-west-2.compute.internal>
2019-01-02 19:03:21 +0000

    Updates

---
+++ precache-manifest.6789d85d619f597f1251b06e31838a6d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8fb2cba9f8e1c2240bc",
+    "url": "/ReactSongApp/static/js/runtime~main.f8fb2cba.js"
+  },
+  {
+    "revision": "34e510564f0c426e5f9d",
+    "url": "/ReactSongApp/static/js/main.34e51056.chunk.js"
+  },
+  {
+    "revision": "4adf92b735ff8937aefd",
+    "url": "/ReactSongApp/static/js/1.4adf92b7.chunk.js"
+  },
+  {
+    "revision": "63993495cb286bd863c10d6cb2ae2880",
+    "url": "/ReactSongApp/index.html"
+  }
+];
